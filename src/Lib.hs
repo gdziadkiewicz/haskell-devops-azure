8fcf8c12944347cd18e473aae278858bab519777
Grzegorz Dziadkiewicz <grzegorzdziadkiewicz@gmail.com>
2023-01-01 21:59:27 +0100

    add MyJson

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,26 @@
 module Lib
     ( someFunc
     ) where
+import Test.QuickCheck
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data MyJson =
+      MyNull
+    | MyBool Bool
+    | MyString String
+    | MyInt Int
+    | MyArray [MyJson]
+    | MyDict [(String,MyJson)]
+    deriving (Show,Eq)
+instance Arbitrary MyJson where
+    arbitrary =
+        oneof [
+            return MyNull,
+            MyBool <$> arbitrary,
+            MyString <$> arbitrary,
+            MyInt <$> arbitrary,
+            MyArray <$> arbitrary,
+            MyDict <$> arbitrary
+        ]
